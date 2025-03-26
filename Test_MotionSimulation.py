b9709611335b7825715ecdea5a4d7d01342f6087
briannghiem <brian.nghiem@mail.utoronto.ca>
2025-03-26 11:42:21 -0400

    temporarily make CUDA invisible, loading all objects to CPU

--- Test_MotionSimulation.py
+++ Test_MotionSimulation.py
@@ -1,27 +1,28 @@
 '''
-Created December 10, 2024
+Created March 26, 2025
 
-Profiling loss functions for "multi-modal" image registration
-Of k-space segments (shots)
+Script for generating motion simulation for a test case,
+using publicly available data from the Calgary-Campinas dataset
 
-Goal is to assess the loss landscape --> is my idea even viable?
-
-This is a script for sliding window recons of segments of k-space
 '''
 
 import os
-# os.environ['LD_LIBRARY_PATH'] ="/home/nghiemb/miniconda3/envs/GenPyMoCo_PTTF_env/lib"
 os.environ['LD_LIBRARY_PATH'] ="/home/nghiemb/miniconda3/envs/GenPyMoCo_PTTF_BFGS_env/lib"
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '' #TEMPORARILY MAKE GPU INVISIBLE
 
 import numpy as np
 
 import matplotlib.pyplot as plt
 import pathlib as plib
+from time import time
 
 import torch
-# torch.cuda.is_available() #check if torch is using gpu acceleration
+if torch.cuda.is_available():#check if torch is using gpu acceleration
+    device = 'cuda'
+else:
+    device = 'cpu'
 
 import encode.encode_op as eop
 import recon.recon_op as rec
@@ -48,9 +49,8 @@ C = np.load(sub_path + r'/sens.npy')
 res = np.array([1,1,1]) #image resolution: 1 mm iso
 
 #Convert to torch tensor
-m_GT = torch.from_numpy(m_GT).to('cuda')
-C = torch.from_numpy(C).to('cuda')
-
+m_GT = torch.from_numpy(m_GT).to(device)
+C = torch.from_numpy(C).to(device)
 
 mask_bkg = rec.getMask(C)
 
@@ -63,153 +63,59 @@ img_dims = [Nx,Ny,Nz]
 #-------------------------------MOTION SIMULATION-------------------------------
 #%%-----------------------------------------------------------------------------
 
-U = np.load(root + r'/U.npy')
-Mtraj_GT = np.load(test_path + r'/Mtraj.npy')
-nshots = U.shape[0]
+#Generating discrete random motion trajectory
+mild_specs = {'Tx':[0.2,0.1],'Ty':[0.4,0.2],'Tz':[0.4,0.2],\
+            'Rx':[0.5,0.2],'Ry':[0.2,0.1],'Rz':[0.2,0.1]} #[max_rate, prob]
+moderate_specs = {'Tx':[0.4,0.15],'Ty':[0.9,0.3],'Tz':[0.9,0.3],\
+            'Rx':[1,0.3],'Ry':[0.5,0.15],'Rz':[0.5,0.15]} #[max_rate, prob]
+severe_specs = {'Tx':[0.8,0.3],'Ty':[1.8,0.6],'Tz':[1.8,0.6],\
+            'Rx':[2,0.6],'Ry':[1.0,0.3],'Rz':[1.0,0.3]} #2x the max_rate and probability
+extreme_specs = {'Tx':[1.6,0.6],'Ty':[3.6,1.0],'Tz':[3.6,1.0],\
+            'Rx':[4,1.0],'Ry':[2.0,0.6],'Rz':[2.0,0.6]} #4x the probability
+motion_specs = {'mild':mild_specs,'moderate':moderate_specs,\
+                'severe':severe_specs, 'extreme':extreme_specs}
 
-s_corrupted = np.load(test_path + r'/s_corrupted.npy')
-m_corrupted = np.load(test_path + r'/m_corrupted.npy')
+#Generate sequential sampling pattern
+pattern = "sequential" #interleaved or sequential
+Rs = (1,1) #SENSE acceleration factor along PE1 and PE2
+TR_shot = 16 #number of TRs per shot
+U_array = np.transpose(msi.make_samp(np.transpose(m_GT.cpu(), (1,0,2)), \
+                                     Rs, TR_shot, order=pattern), \
+                                        (0,2,1,3)).astype(np.float32)
+U = torch.from_numpy(U_array).to(device U_array
 
+#Generate motion parameters
+j = 1; k = 1 #legacy parameters, can have placeholder values of 1; TO DO --> REMOVE THESE PARAMS
+rand_keys = msi._gen_key(sub, j, k)
+motion_lv = 'moderate'
+nshots = U.shape[0] #PE1 along Ny in this case
 
-U = torch.from_numpy(U).to('cuda')
-Mtraj_GT = torch.from_numpy(Mtraj_GT).to('cuda')
-
-s_corrupted = torch.from_numpy(s_corrupted).to('cuda')
-m_corrupted = torch.from_numpy(m_corrupted).to('cuda')
-
-# plot_views(abs(m_GT).detach().cpu(), 'auto')
-# plot_views(abs(m_corrupted).detach().cpu(), 'auto')
-
+Mtraj_GT = torch.tensor(msi._gen_traj(rand_keys, motion_lv, nshots, motion_specs)).to(device)
 
 T_GT = [Mtraj_GT[:,0], Mtraj_GT[:,1], Mtraj_GT[:,2]]
 R_GT = [Mtraj_GT[:,3], Mtraj_GT[:,4], Mtraj_GT[:,5]]
-# plot_Mtraj(T_GT, R_GT, T_GT, R_GT, img_dims, rescale = 0)
+
+T0 = [Mtraj_GT[:,0]*0, Mtraj_GT[:,1]*0, Mtraj_GT[:,2]*0]
+R0 = [Mtraj_GT[:,3]*0, Mtraj_GT[:,4]*0, Mtraj_GT[:,5]*0]
+
+plot_Mtraj(T_GT, R_GT, T0, R0, img_dims) #Plotting motion trajectories (groundtruth vs initial)
 
 
-#%%-----------------------------------------------------------------------------
-#---------------------------------PROFILING UNet--------------------------------
-#%%-----------------------------------------------------------------------------
-
-wpath_SAPUNet = r'/home/nghiemb/GenPyMoCo/cnn/weights/_archived'
-pads = [11,3]
-
-UNet_params = [wpath_SAPUNet, pads]
-m_est = m_corrupted
-from time import time
 t1 = time()
-output_UNet = rec.UNet_magnitude(m_est, UNet_params)
+s_corrupted = eop._E(m_GT,C,U,T_GT,R_GT,res) 
 t2 = time()
 print("Elapsed time: {} sec".format(t2 - t1))
-# m_out_UNet = output_UNet[0]
-# plot_views(abs(m_out_UNet).detach().cpu(), 'auto')
 
-#%%-----------------------------------------------------------------------------
-#----------------------------------PROFILING JE---------------------------------
-#%%-----------------------------------------------------------------------------
+t3 = time()
+CG_mask=None
+CG_maxiter=5 #heuristically determined...
+CG_nbatch=1 #batch for calls to E or EH
+CG_result = "optimal"
+CG_params = [CG_mask, CG_maxiter, CG_nbatch, CG_result]
+m_corrupted, _, _ = rec.ImageEstimation(s_corrupted,C,U,T0,R0,res,CG_params)
+t4 = time()
+print("Elapsed time: {} sec".format(t4 - t3))
 
-offset = 0
-Tx_init = torch.zeros(nshots)+offset; Tx_init[0] = 0
-Ty_init = torch.zeros(nshots)+offset; Ty_init[0] = 0
-Tz_init = torch.zeros(nshots)+offset; Tz_init[0] = 0
-Rx_init = torch.zeros(nshots)+offset; Rx_init[0] = 0
-Ry_init = torch.zeros(nshots)+offset; Ry_init[0] = 0
-Rz_init = torch.zeros(nshots)+offset; Rz_init[0] = 0
-T_init = [Tx_init.to('cuda'), Ty_init.to('cuda'), Tz_init.to('cuda')]
-R_init = [Rx_init.to('cuda'), Ry_init.to('cuda'), Rz_init.to('cuda')]
-
-T = T_init
-R = R_init
-
-JE_maxiters = 10
-ME_params = ['Adam', 'DC', 1, 1, 0.05]
-CG_params = [None, 5, 1, 'optimal']
-g_window = 10
-JE_params = [ME_params, CG_params, JE_maxiters, g_window]
-m_est = m_corrupted
-output_JE = rec.JE(m_est, s_corrupted, C, \
-                   U, T, R, res, JE_params)
-
-m_out_JE = output_JE[0]
-T_JE = output_JE[1]
-R_JE = output_JE[2]
-
-plot_views(abs(m_out_JE).detach().cpu(), 'auto')
-plot_Mtraj(T_GT, R_GT, T_JE, R_JE, img_dims, rescale = 0)
-
-
-DC_list_JE = [val.detach().cpu().numpy() for val in output_JE[-1]]
-DC_array_JE = np.array(DC_list_JE)
-iter_array = np.arange(0, JE_maxiters+1)
-
-plt.figure()
-plt.plot(iter_array, DC_array_JE)
-plt.xlabel("Iteration")
-plt.ylabel("Total DC loss")
-plt.title("Total DC Loss Across {} JE Iterations".format(JE_maxiters))
-plt.show()
-
-
-'''
-from torchmin import minimize
-from torchmin.benchmarks import rosen
-
-def _f(x):
-    return sum(x.flatten())**2 +1
-
-# initial point
-x0 = torch.zeros(2,2).to("cuda")
-
-# BFGS
-result = minimize(_f, x0, method='bfgs')
-
-# Newton Conjugate Gradient
-result = minimize(rosen, x0, method='newton-cg')
-'''
-
-#%%-----------------------------------------------------------------------------
-#-------------------------------PROFILING UNet+JE-------------------------------
-#%%-----------------------------------------------------------------------------
-
-offset = 0
-Tx_init = torch.zeros(nshots)+offset; Tx_init[0] = 0
-Ty_init = torch.zeros(nshots)+offset; Ty_init[0] = 0
-Tz_init = torch.zeros(nshots)+offset; Tz_init[0] = 0
-Rx_init = torch.zeros(nshots)+offset; Rx_init[0] = 0
-Ry_init = torch.zeros(nshots)+offset; Ry_init[0] = 0
-Rz_init = torch.zeros(nshots)+offset; Rz_init[0] = 0
-T_init = [Tx_init.to('cuda'), Ty_init.to('cuda'), Tz_init.to('cuda')]
-R_init = [Rx_init.to('cuda'), Ry_init.to('cuda'), Rz_init.to('cuda')]
-
-T = T_init
-R = R_init
-
-wpath_SAPUNet = r'/home/nghiemb/GenPyMoCo/cnn/weights/_archived'
-pads = [11,3]
-
-UNetJE_maxiters = 10
-ME_params = ['Adam', 'DC', 1, 1, 0.05]
-CG_params = [None, 5, 1, 'optimal']
-g_window = 10
-JE_params = [ME_params, CG_params, JE_maxiters, g_window]
-UNet_params = [wpath_SAPUNet, pads]
-m_est = m_corrupted
-output_UNetJE = rec.UNetJE(m_est, s_corrupted, C, \
-                           U, T, R, res, JE_params, UNet_params)
-
-m_out_UNetJE = output_UNetJE[0]
-T_UNetJE = output_UNetJE[1]
-R_UNetJE = output_UNetJE[2]
-
-plot_views(abs(m_out_UNetJE).detach().cpu(), 'auto')
-plot_Mtraj(T_GT, R_GT, T_UNetJE, R_UNetJE, img_dims, rescale = 0)
-
-DC_list_UNetJE = [val.detach().cpu().numpy() for val in output_UNetJE[-1]]
-DC_array_UNetJE = np.array(DC_list_UNetJE)
-iter_array = np.arange(0, UNetJE_maxiters+1)
-
-plt.figure()
-plt.plot(iter_array, DC_array_UNetJE)
-plt.xlabel("Iteration")
-plt.ylabel("Total DC loss")
-plt.title("Total DC Loss Across {} UNet+JE Iterations".format(UNetJE_maxiters))
-plt.show()
+# torch.save(Mtraj_GT, dpath + "/Mtraj_GT_seq_mild.pt")
+# torch.save(s_corrupted, dpath + "/s_corrupted_seq_mild.pt")
+# torch.save(m_corrupted, dpath + "/m_corrupted_seq_mild.pt")
